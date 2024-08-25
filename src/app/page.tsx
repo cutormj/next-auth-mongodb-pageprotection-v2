76df323f9fe2a16455119c149c6e4f056fc62dec
Joseph Cutor <cutormarkjoseph@gmail.com>
2024-08-25 23:11:14 +0800

    root update instructions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center m-4">
       <h1 className="text-3xl my-3">Start Here!</h1>
+      <code className="text-red-500 underline">You are in <b>Default Template</b>!</code>
+      <span>Clone me and Create a new Branch</span>
       <LoginForm />
       {/* <p className="my-3">
         Dont you have an account?
